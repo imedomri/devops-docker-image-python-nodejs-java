89b97eb3b53a67a7e1718db8248435dcc802c062
imedomri <imedomri.devops@gmail.com>
2022-04-04 04:21:52 +0100

    add project java spring boot

---
+++ hello-world-java/src/test/java/com/oidevops/rest/webservices/restfulwebservices/HelloWorldJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oidevops.rest.webservices.restfulwebservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloWorldJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
